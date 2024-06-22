42f6856a65513461396b4eafeec90cc21d77e64e
Neto test <bosgoty@mail.ru>
2024-06-22 23:46:12 +0700

    Improved test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -17,15 +18,18 @@ public class CardDeliveryTest {
 
         LocalDate currentDate = LocalDate.now();
         LocalDate inFiveDays = currentDate.plusDays(6);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        String text = dtf.format(inFiveDays);
+        DateTimeFormatter normalData = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String text = normalData.format(inFiveDays);
         $("[placeholder='Дата встречи']").doubleClick();
-        $("[placeholder='Дата встречи']").sendKeys(" ");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
         $("[placeholder='Дата встречи']").setValue(text);
 
         $("[name='phone']").setValue("+79344582812");
         $(".checkbox__box").click();
         $(".button__content").click();
-        $(".notification__title").shouldHave(Condition.exactText("Успешно!"), Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldBe((Condition.visible), Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + text));
+
     }
 }
